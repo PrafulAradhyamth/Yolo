3e6d08f4c3b6949f8914b52bd9eae8185b169921
PRAFUL ARADHYAMATH <60805443+PrafulAradhyamth@users.noreply.github.com>
2020-09-12 11:29:51 +0530

    Add files via upload

---
+++ project/yolo more.py
@@ -0,0 +1,78 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pyttsx3
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+
+vidfeed = cv2.VideoCapture(0)
+
+def getFrame(sec):
+    vidfeed.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidfeed.read()
+    if hasFrames:
+         # save frame as JPG file
+        
+        blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        net.setInput(blob)
+        outs = net.forward(output_layers)
+        img = cv2.resize(image, None, fx=1, fy=1,interpolation=cv2.INTER_AREA)
+        height, width, channels = img.shape
+        
+
+# Showing informations on the screen
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+            # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+            # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        print(indexes)
+        font = cv2.FONT_HERSHEY_PLAIN
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                print(label)
+                engine = pyttsx3.init()
+                engine.say(label)
+                engine.runAndWait()
+                color = colors[i]
+                cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
+        cv2.imshow("image",img)
+        return hasFrames
+
+sec = 0
+frameRate = 0.25 #//it will capture image in each 0.5 second
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
